e163be1247d1d93dae0a944fdc2a066374169a66
wolfsilver <wolfsilver@users.noreply.github.com>
2026-04-21 23:10:07 +0800

    荣耀智慧空间广告

---
+++ src/apps/com.hihonor.magichome.ts
@@ -0,0 +1,23 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.hihonor.magichome',
+  name: '荣耀智慧空间',
+  groups: [
+    {
+      key: 1,
+      name: '开屏广告',
+      fastQuery: true,
+      activityIds: '.activity.WelcomeActivity',
+      matchTime: 10000,
+      actionMaximum: 1,
+      rules: [
+        {
+          key: 0,
+          matches: 'id="com.hihonor.magichome:id/ads_splash_countdown"',
+          snapshotUrls: 'https://i.gkd.li/snapshot/1776783579067',
+        },
+      ],
+    },
+  ],
+});
